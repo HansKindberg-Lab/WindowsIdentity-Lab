9c39b65261f16129be2509084a06f3e4d251f161
Hans Kindberg <hans.kindberg@gmail.com>
2015-09-25 13:22:05 +0200

    Add solution

---
+++ IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Integration-tests for HansKindberg.")]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("d954daa5-1a13-4c3b-a956-0ef7d76d6b1e")]
+
+// ReSharper disable CheckNamespace
+internal static class AssemblyInfo // ReSharper restore CheckNamespace
+{
+	#region Fields
+
+	internal const string AssemblyName = "HansKindberg.IntegrationTests";
+
+	#endregion
+}
---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Unit-tests for HansKindberg.")]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("f6aec151-81a7-4127-aa29-8fe431cc4ac8")]
+
+// ReSharper disable CheckNamespace
+internal static class AssemblyInfo // ReSharper restore CheckNamespace
+{
+	#region Fields
+
+	internal const string AssemblyName = "HansKindberg.UnitTests";
+
+	#endregion
+}
